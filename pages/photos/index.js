6c3aceed8d854158749819da3a1e72b3807b4d39
Vincent Budrovich <vwb@berkeley.edu>
2020-02-15 19:18:23 -0800

    include tailwindcss

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -2,9 +2,17 @@ import { PHOTO_DATA } from "../../data/images";
 
 const PhotosIndex = () => {
     return (
-        <div>
+        <div className="flex flex-wrap content-center">
             {PHOTO_DATA.map(img => (
-                <img key={img} src={img} />
+                <div
+                    className="w-full sm:w-1/2 lg:w-1/4 p-4 self-center"
+                    key={img}
+                >
+                    <img
+                        className="mx-auto align-middle bg-grey-100"
+                        src={img}
+                    />
+                </div>
             ))}
         </div>
     );
