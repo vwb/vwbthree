bb79cbe1f4c541860306e108391204de46f90169
Vincent Budrovich <vwb@berkeley.edu>
2020-02-19 22:16:29 -0800

    add back the proper styling to blog posts

--- components/Markdown.js
+++ components/Markdown.js
@@ -15,6 +15,37 @@ const Markdown = ({ content }) => {
                     overflow: hidden;
                 }
             `}</style>
+            <style jsx global>{`
+                pre {
+                    overflow: auto;
+                    background-color: #f6f8fa;
+                    border-radius: 3px;
+                    font-size: 85%;
+                    line-height: 1.45;
+                    overflow: auto !important;
+                    padding: 16px;
+                }
+                pre code {
+                    background-color: transparent;
+                    border: 0;
+                    display: inline;
+                    line-height: inherit;
+                    margin: 0;
+                    max-width: auto;
+                    overflow: visible;
+                    padding: 0;
+                    word-wrap: normal;
+                }
+                pre > code {
+                    background: transparent;
+                    border: 0;
+                    font-size: 100%;
+                    margin: 0;
+                    padding: 0;
+                    white-space: pre;
+                    word-break: normal;
+                }
+            `}</style>
         </>
     );
 };
