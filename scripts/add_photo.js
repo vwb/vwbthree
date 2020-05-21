5a7f50dfd31f400b93d3bdfb4997a9d759907872
Vincent B <vinchenzio@gmail.com>
2020-05-20 17:42:36 -0700

    add many more photos

--- scripts/add_photo.js
+++ scripts/add_photo.js
@@ -1,106 +1,37 @@
-// Move DATA files to JSON
-const fs = require("fs");
+const { addPhoto, parseArgs } = require("./utils");
 
 // Handle arguments
 const myArgs = process.argv.slice(2);
-
-const parseArgs = args =>
-    args.reduce((memo, arg) => {
-        [key, val] = arg.split("=");
-
-        const trimmedKey = key.slice(2, key.length);
-
-        return {
-            ...memo,
-            [trimmedKey]: val
-        };
-    }, {});
 
 function main() {
     const parsedArgs = parseArgs(myArgs);
 
     if (
-        Object.keys(parsedArgs).indexOf("url") === -1 ||
-        Object.keys(parsedArgs).indexOf("title") === -1
+        Object.keys(parsedArgs).indexOf("title") === -1 ||
+        Object.keys(parsedArgs).indexOf("collection") === -1
     ) {
         console.log(
-            "Error with passed in argument. 'url' and 'title' are required arguments."
+            "Error with passed in argument. 'title' is a required argument."
         );
         return;
     }
 
-    addPhoto(parsedArgs);
-
-    console.log(`Successfully added photo: ${parsedArgs.title} to images.json`);
-
-    if (parsedArgs.collection) {
+    if (parsedArgs.title.indexOf(",") > -1) {
+        parsedArgs.title.split(",").map(title => {
+            addPhoto({ title, collection: parsedArgs.collection });
+            console.log(
+                `Successfully added photo: ${parsedArgs.title} to images.json`
+            );
+            console.log(
+                `Successfully added photo: ${parsedArgs.title} to ${parsedArgs.collection}`
+            );
+        });
+    } else {
+        addPhoto(parsedArgs);
         console.log(
-            `Successfully added photo: ${parsedArgs.title} to ${parsedArgs.collection}`
+            `Successfully added photo: ${parsedArgs.title} to images.json`
         );
     }
 }
-
-const addPhoto = ({ url, title, collection }) => {
-    const images = loadImages();
-
-    const photoId = Object.keys(images).length;
-
-    const photo = {
-        url,
-        title,
-        id: photoId
-    };
-
-    const updatedImages = {
-        ...images,
-        [photoId]: photo
-    };
-
-    saveImages(updatedImages);
-
-    if (collection) {
-        const collections = loadCollections();
-
-        try {
-            const collectionToUpdate = collections[collection];
-
-            const updatedCollection = {
-                ...collectionToUpdate,
-                ids: [...collectionToUpdate.ids, photoId]
-            };
-
-            const updatedCollections = {
-                ...collections,
-                [collection]: updatedCollection
-            };
-
-            saveCollection(updatedCollections);
-        } catch (e) {
-            console.log(`Passed in collection: ${collection} doesn't exist`);
-        }
-    }
-};
-
-//File Getters and Parsers
-const IMAGE_FILE_PATH = "../data/photos/images.json";
-const COLLECTIONS_FILE_PATH = "../data/photos/collections.json";
-
-const loadImages = () => loadData({ filePath: IMAGE_FILE_PATH });
-const loadCollections = () => loadData({ filePath: COLLECTIONS_FILE_PATH });
-
-const saveImages = data => saveData({ filePath: IMAGE_FILE_PATH, data });
-const saveCollection = data =>
-    saveData({ filePath: COLLECTIONS_FILE_PATH, data });
-
-const saveData = ({ filePath, data }) => {
-    const dataJSON = JSON.stringify(data);
-    fs.writeFileSync(filePath, dataJSON);
-};
-
-const loadData = ({ filePath }) => {
-    const dataBuffer = fs.readFileSync(filePath);
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
-};
 
 main();
---
+++ scripts/utils.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const parseArgs = args =>
+    args.reduce((memo, arg) => {
+        [key, val] = arg.split("=");
+
+        const trimmedKey = key.slice(2, key.length);
+
+        return {
+            ...memo,
+            [trimmedKey]: val
+        };
+    }, {});
+
+const IMAGE_FILE_PATH = "../data/photos/images.json";
+const COLLECTIONS_FILE_PATH = "../data/photos/collections.json";
+
+const loadImages = () => loadData({ filePath: IMAGE_FILE_PATH });
+const loadCollections = () => loadData({ filePath: COLLECTIONS_FILE_PATH });
+
+const saveImages = data => saveData({ filePath: IMAGE_FILE_PATH, data });
+const saveCollection = data =>
+    saveData({ filePath: COLLECTIONS_FILE_PATH, data });
+
+const saveData = ({ filePath, data }) => {
+    const dataJSON = JSON.stringify(data);
+    fs.writeFileSync(filePath, dataJSON);
+};
+
+const loadData = ({ filePath }) => {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+};
+
+const AWS_ROOT = "https://vwbthree-photos.s3-us-west-1.amazonaws.com";
+const generateUrl = (name, collection) =>
+    `${AWS_ROOT}/${collection}/${name}.jpg`;
+const stringifyTitle = name =>
+    name
+        .split("-")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+
+const addPhoto = ({ title, collection }) => {
+    const images = loadImages();
+    const photoValues = Object.values(images);
+    const photoId = photoValues[photoValues.length - 1].id + 1;
+
+    const photo = {
+        url: generateUrl(title, collection),
+        title: stringifyTitle(title),
+        id: photoId
+    };
+
+    const updatedImages = {
+        ...images,
+        [photoId]: photo
+    };
+
+    saveImages(updatedImages);
+
+    if (collection) {
+        const collections = loadCollections();
+
+        try {
+            const collectionToUpdate = collections[collection];
+
+            const updatedCollection = {
+                ...collectionToUpdate,
+                ids: [...collectionToUpdate.ids, photoId]
+            };
+
+            const updatedCollections = {
+                ...collections,
+                [collection]: updatedCollection
+            };
+
+            saveCollection(updatedCollections);
+        } catch (e) {
+            console.log(`Passed in collection: ${collection} doesn't exist`);
+        }
+    }
+};
+
+module.exports = {
+    addPhoto,
+    parseArgs
+};
