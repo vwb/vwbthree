f9364b6a022161d09a849514138637d30db44dfd
Vincent Budrovich <vwb@berkeley.edu>
2020-05-26 18:06:13 -0700

    Integrate Better Panel (#6)
    * better panel
    
    * clean up copy
    
    Co-authored-by: Vincent B <vinchenzio@gmail.com>

---
+++ components/Divider.js
@@ -0,0 +1,5 @@
+const Divider = () => (
+    <div className="w-full mx-auto border border-gray-400 border-solid" />
+);
+
+export default Divider;
---
+++ components/Panel.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export const PanelContext = React.createContext(false);
+
+const Panel = props => {
+    const [isOpen, setOpen] = useState(false);
+    const CLOSED_DETAIL_PANEL_STYLES = {
+        height: "64px",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        transition: "height 0.4s"
+    };
+
+    const OPEN_DETAIL_PANEL_STYLES = {
+        ...CLOSED_DETAIL_PANEL_STYLES,
+        height: "75vh"
+    };
+
+    return (
+        <div
+            className="z-20 fixed bg-opacity-0"
+            style={
+                isOpen ? OPEN_DETAIL_PANEL_STYLES : CLOSED_DETAIL_PANEL_STYLES
+            }
+        >
+            <div
+                style={{
+                    backgroundColor: `rgba(30,30,30, ${isOpen ? 1 : 0.6})`,
+                    transition: "background-color 0.4s"
+                }}
+                className="w-full sm:w-3/4 lg:w-1/2 xl:x-1/3 mx-auto h-full rounded-lg rounded-b-none shadow-xl text-gray-200"
+            >
+                <PanelContext.Provider value={[isOpen, setOpen]}>
+                    {props.children}
+                </PanelContext.Provider>
+            </div>
+        </div>
+    );
+};
+
+export default Panel;
