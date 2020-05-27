52cee807d471fcbe84bbb4d0008b3ba2ecf27a49
Vincent B <vinchenzio@gmail.com>
2020-05-26 18:54:56 -0700

    fix up purchase flow

--- pages/photos/[id]/[slug].js
+++ pages/photos/[id]/[slug].js
@@ -5,9 +5,6 @@ import Panel, { PanelContext } from "../../../components/Panel";
 import Divider from "../../../components/Divider";
 import { getAllPhotoPaths, getPhotoData } from "../../../lib/photos";
 
-const FAKE_DESCRIPTION =
-    "Just my luck, no ice. Eventually, you do plan to have dinosaurs on your dinosaur tour, right? Did he just throw my cat out of the window? This thing comes fully loaded. AM/FM radio, reclining bucket seats, and... power windows. Jaguar shark! So tell me - does it really exist?";
-
 const DetailHeader = props => (
     <div className="mx-4">
         <div
@@ -20,7 +17,7 @@ const DetailHeader = props => (
     </div>
 );
 
-const BackGround = props => (
+const Background = props => (
     <div className="w-full h-screen">
         <div style={{ height: "100%" }} className="w-full fixed">
             <div className="flex items-center overflow-hidden relative h-full">
@@ -45,46 +42,80 @@ const FadeIn = props => {
     );
 };
 
-const ContactForm = () => {
+const ContactForm = props => {
+    const [emailInput, setEmailInput] = useState("");
+    const [isSubmitted, setSubmitted] = useState(false);
+
     const handleSubmit = e => {
         e.preventDefault;
+
+        //TODO: Contact Form
+        //validate input is a valid email
+
+        //post at to be created endpoint
+
+        //store "submitted:photo-id" in local storage and
+        //leverage that value for usestate on mount
 
         alert(
             "Thank you for your interest. \n This feature is still in development, please try again soon"
         );
+
+        setSubmitted(true);
     };
 
     return (
         <section className="pt-4 px-4 w-full sm:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto">
-            <form
-                className="flex flex-col items-center"
-                onSubmit={handleSubmit}
-            >
-                <p className="text-center">
-                    Enter your email below to be contacted regarding purchasing
-                    options
-                </p>
-                <div className="w-full flex px-4">
-                    <label htmlFor="email-input" className="hidden">
-                        Email
-                    </label>
-                    <input
-                        id="email-input"
-                        className="p-3 mt-4 flex-grow rounded text-gray-800 focus:outline-none focus:border-teal-200"
-                        type="text"
-                        placeholder="example@domain.com"
-                    />
-                </div>
-                <p className="text-xs pt-2">
-                    Digital download and custom prints available
-                </p>
-                <button
-                    className="w-3/4 mt-5 p-3 hover:shadow-xl text-gray-300 focus:outline-none bg-teal-700 rounded"
-                    type="submit"
+            {!isSubmitted && (
+                <form
+                    className="flex flex-col items-center"
+                    onSubmit={handleSubmit}
                 >
-                    Submit
-                </button>
-            </form>
+                    <p className="text-center">
+                        Enter your email below to be contacted regarding
+                        purchasing options
+                    </p>
+                    <div className="w-full flex px-4">
+                        <label htmlFor="email-input" className="hidden">
+                            Email
+                        </label>
+                        <input
+                            id="email-input"
+                            className="p-3 mt-4 flex-grow rounded text-gray-800 focus:outline-none"
+                            type="text"
+                            placeholder="example@domain.com"
+                            value={emailInput}
+                            onChange={e => setEmailInput(e.target.value)}
+                        />
+                    </div>
+                    <p className="text-xs pt-2">
+                        Digital download and custom prints available
+                    </p>
+                    <button
+                        className="w-3/4 mt-5 p-3 hover:shadow-xl text-gray-300 focus:outline-none bg-teal-700 rounded"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+            )}
+            {isSubmitted && (
+                <section className="flex flex-col items-center pt-4">
+                    <p className="text-lg font-semibold text-center">
+                        Thank you for your interest!
+                    </p>
+                    <p className="text-center test-sm pt-2">
+                        Keep an eye on your email and I will reaching out in the
+                        next few days.
+                    </p>
+                    <button
+                        className="p-3 my-4 border-2 border-solid border-gray-400 rounded"
+                        onClick={() => props.setPanelOpen(false)}
+                    >
+                        Close
+                    </button>
+                </section>
+            )}
         </section>
     );
 };
@@ -101,7 +132,7 @@ const PanelContent = props => {
                         <button
                             className={`${
                                 isPanelOpen
-                                    ? "bg-transparent"
+                                    ? "bg-transparent mt-2"
                                     : "bg-teal-700 rounded"
                             }   p-3 hover:shadow-xl text-gray-300 focus:outline-none`}
                             style={{ transition: "all .4s" }}
@@ -116,7 +147,7 @@ const PanelContent = props => {
                 <div className="pt-4">
                     <FadeIn>
                         <Divider />
-                        <ContactForm />
+                        <ContactForm setPanelOpen={setPanelOpen} />
                     </FadeIn>
                 </div>
             )}
@@ -127,7 +158,7 @@ const PanelContent = props => {
 const PhotoDetailPage = ({ photo }) => {
     return (
         <Layout isOpenDefault={false} navClass="bg-transparent text-gray-200">
-            <BackGround>
+            <Background>
                 <PhotoView
                     photo={photo}
                     backgroundOverlay="rgba(15, 15, 15, 0.65), rgba(15, 15, 15, 0.65))"
@@ -135,7 +166,7 @@ const PhotoDetailPage = ({ photo }) => {
                         <div style={{ marginTop: "-50px" }}>{image}</div>
                     )}
                 />
-            </BackGround>
+            </Background>
             <Panel>
                 <PanelContent photo={photo} />
             </Panel>
