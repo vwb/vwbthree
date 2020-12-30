f156f99e04d84e6c87eef1c957b2c9b20d32b54c
Vincent Budrovich <vwbthree@gmail.com>
2020-12-29 18:53:46 -0800

    test start

--- pages/photos/[id]/index.js
+++ pages/photos/[id]/index.js
@@ -89,7 +89,7 @@ const PhotoGroupPage = props => {
 
             if (potentialIndex) {
                 if (potentialIndex?.collectionName === props.collection.name) {
-                    ref?.current?.scrollToItem(potentialIndex.index, "end");
+                    ref?.current?.scrollToItem(potentialIndex.index, "start");
                 }
 
                 if (ref.current) {
