bbb2f13864fbbc85325983380ac5b080604e27b6
Vincent Budrovich <vwbthree@gmail.com>
2022-12-12 08:08:09 -0800

    ensure mobile works

---
+++ pages/head.jsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <title>Vincent Budrovich</title>
+        </>
+    );
+}
