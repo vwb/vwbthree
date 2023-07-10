a70683a65ff18fa9d4f7546a6972483f992dcaa1
Vincent Budrovich <vwbthree@gmail.com>
2023-07-10 11:49:56 -0700

    extend delayed resp slightly to ensure fetch fires before container closes

--- pages/api/checkout/updates/index.js
+++ pages/api/checkout/updates/index.js
@@ -83,7 +83,7 @@ export default async function handler(req, res) {
                 async function delayResp() {
                     return setTimeout(() => {
                         res.json({ received: true });
-                    }, 200);
+                    }, 400);
                 }
 
                 res.status(200);
